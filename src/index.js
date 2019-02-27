3c23737c33711e1e02c4ded0cc53ff4e131f15fe
Olga Luk <olga_ivanova_86@mail.ru>
2019-02-27 21:39:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  const initialValue = currency;
+  const boxValue = {};
+  if (initialValue > 0 && initialValue < 10000) {
+    boxValue.H = ~~(initialValue / 50);
+    boxValue.Q = ~~((initialValue % 50)/25);
+    boxValue.D = ~~(((initialValue % 50)%25)/10);
+    boxValue.N = ~~((((initialValue % 50)%25)%10)/5);
+    boxValue.P = ~~((((initialValue % 50)%25)%10)%5);
+    for (let key in boxValue) {
+      if (!boxValue[key]) {
+        delete boxValue[key];
+      }
+    }
+  }
+  if (initialValue >= 10000) {
+    boxValue.error = "You are rich, my friend! We don't have so much coins for exchange";
+  }
+  return boxValue;
 }
